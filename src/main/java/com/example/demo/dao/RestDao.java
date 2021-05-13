7b0ce9c50c0bdbd65d9f64963751546b81a7240c
rojimo <rocio.jimenezmoreno8@gmail.com>
2021-05-13 19:00:43 +0200

    proyecto rest con el metodo listProjectRest()

--- src/main/java/com/example/demo/dao/RestDao.java
+++ src/main/java/com/example/demo/dao/RestDao.java
@@ -1,9 +1,10 @@
 package com.example.demo.dao;
 
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
-
 import com.example.demo.model.Project;
 
 public interface RestDao extends JpaRepository<Project, Integer> {
 
+	//public List<Project> getProjects();
 }
--- src/main/java/com/example/demo/service/RestServiceImpl.java
+++ src/main/java/com/example/demo/service/RestServiceImpl.java
@@ -2,8 +2,9 @@ package com.example.demo.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
 import com.example.demo.dao.RestDao;
+import java.util.List;
+import com.example.demo.model.Project;
 
 @Service
 public class RestServiceImpl implements RestService{
@@ -11,4 +12,8 @@ public class RestServiceImpl implements RestService{
 	@Autowired
 	RestDao restDao;
 	
+	@Override
+	public List<Project> getProjects() {
+		return restDao.findAll();
+	}
 }
